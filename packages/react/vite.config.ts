afb34b82079fa533d82d3fbfd79f66cbbde6ac99
Dominic Tobias <dominic.tobias@gmail.com>
2023-12-31 20:45:25 +0000

    Try and fix build issues

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -4,31 +4,41 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   server: {
     port: 8094,
   },
   plugins: [tsconfigPaths(), react()],
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/lib.ts'),
-      name: 'LightgridReact',
-      // the proper extensions will be added
-      fileName: 'lightgrid-react',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react', 'react-dom'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-          reactDOM: 'ReactDOM',
+})
+
+console.log('process.env.BUILD_MODE', process.env.BUILD_MODE)
+
+if (process.env.BUILD_MODE === 'lib') {
+  Object.assign(config, {
+    define: { 'process.env.NODE_ENV': '"production"' },
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: resolve(__dirname, 'src/lib.ts'),
+        name: 'LightgridReact',
+        // the proper extensions will be added
+        fileName: 'index',
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ['react', 'react-dom'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            react: 'React',
+            reactDOM: 'ReactDOM',
+          },
         },
       },
     },
-  },
-})
+  })
+}
+
+export default config
